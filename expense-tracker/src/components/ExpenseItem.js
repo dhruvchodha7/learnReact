32e2751a9249be208f48499acfc1728d79a30993
dhruvchodha7 <dhruvchodha@live.com>
2022-06-27 22:24:10 +0530

    added props in app and ExpenseItem

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
-import "./ExpenseItem.css";
+import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insaurance";
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
-    <div>
-      <div className="expense-item">{expenseDate.toISOString()}</div>
-      <div>
-        <h2 className="expense-item__description">{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
