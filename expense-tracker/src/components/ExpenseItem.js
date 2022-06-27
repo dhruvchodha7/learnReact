33241eb9d378f05624c52fa33ddeeef1d24c8b3c
dhruvchodha7 <dhruvchodha@live.com>
2022-06-27 22:14:46 +0530

    added css to ExpenseItem

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insaurance";
+  const expenseAmount = 294.67;
+
   return (
     <div>
-      <div>Date</div>
+      <div className="expense-item">{expenseDate.toISOString()}</div>
       <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+        <h2 className="expense-item__description">{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
