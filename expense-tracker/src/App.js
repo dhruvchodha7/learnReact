45b62423e54e14ad3e62ac8c559a4c4dc1b56b8b
dhruvchodha7 <dhruvchodha@live.com>
2022-06-28 12:40:54 +0530

    added NewExpense

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expeneses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -26,6 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
