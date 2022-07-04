ed51cea275d605bdac4fea4fe89225f59ba8b7af
dhruvchodha7 <dhruvchodha@live.com>
2022-07-04 15:19:26 +0530

    added error modal

--- 4 user-project/src/components/Users/AddUser.js
+++ 4 user-project/src/components/Users/AddUser.js
@@ -1,40 +1,59 @@
-import { useState } from 'react';
-import Card from '../UI/Card'
-import classes from './AddUser.module.css'
-import Button from '../UI/Button'
-import UsersList from './UsersList';
- const AddUser = props => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+import React, { useState } from "react";
 
-    const addUserHandler = (event) => {
-        event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) return;
-        if(+enteredAge < 1) return;
-        console.log(enteredUsername, enteredAge);
-        setEnteredAge('');
-        setEnteredUsername('');
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
     }
+    if (+enteredAge < 1) {
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    };
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    };
-    return(
-        <Card className={classes.input}>
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <ErrorModal title="An error occured!" message="Something Went Wrong!"/>
+      <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
-            <label htmlFor="age">Age (Years)</label>
-            <input id="username" type="number" value={enteredAge} onChange={ageChangeHandler}/>
-            <Button type="submit">Add User</Button>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
         </form>
-            <UsersList/>
-        </Card>
-    )
- };
+      </Card>
+    </div>
+  );
+};
 
- export default AddUser;
+export default AddUser;
