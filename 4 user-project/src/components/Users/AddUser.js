6ae9e153bd5f20586dc9c6f809d5abb231ba33b6
dhruvchodha7 <dhruvchodha@live.com>
2022-07-04 16:19:49 +0530

    project finally completed

--- 4 user-project/src/components/Users/AddUser.js
+++ 4 user-project/src/components/Users/AddUser.js
@@ -1,31 +1,34 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Card from "../UI/Card";
-import Button from "../UI/Button";
-import classes from "./AddUser.module.css";
-import ErrorModal from "../UI/ErrorModal";
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-const [error, setError] = useState();
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
-        message: 'Please Enter a valid name and age (non empty values)'
-      })
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
+      return;
     }
     if (+enteredAge < 1) {
       setError({
-        title: 'Invalid Age',
-        message: 'Please enter a valid age (>0)'
-      })
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
+      return;
     }
     props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -38,11 +41,17 @@ const [error, setError] = useState();
 
   const errorHandler = () => {
     setError(null);
-  }
+  };
 
   return (
     <div>
-     {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
