bb3418f6c8199b955e6cd673249a8e4f264dc5b9
dhruvchodha7 <dhruvchodha@live.com>
2022-07-11 14:08:59 +0530

    updated cart logic

--- 6 food-order/food-order/src/store/CartProvider.js
+++ 6 food-order/food-order/src/store/CartProvider.js
@@ -7,11 +7,26 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type == "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    if(existingCartItem){
+      const updatedItem={
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }else{
+      updatedItems = state.items.concat(action.item)
+    }
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
     };
   }
   return defaultCartState;
@@ -26,7 +41,6 @@ function CartProvider(props) {
     dispatchCartAction({ type: "ADD", item: Item });
   };
   const removeItemFromCartHandler = (id) => {
-
     dispatchCartAction({ type: "REMOVE", id: id });
   };
   const cartContext = {
