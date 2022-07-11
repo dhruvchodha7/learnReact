37f54c9e5414280a74b7651d63ecf5614bcfdde7
dhruvchodha7 <dhruvchodha@live.com>
2022-07-11 15:30:23 +0530

    food order app finished

--- 6 food-order/food-order/src/components/Cart/Cart.js
+++ 6 food-order/food-order/src/components/Cart/Cart.js
@@ -8,8 +8,13 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
-  const cartItemAddHandler = (item)=>{}
-  const cartItemRemoveHandler = (id)=>{}
+  const cartItemAddHandler = (item)=>{
+    cartCtx.addItem(item);
+  }
+  const cartItemRemoveHandler = (id)=>{
+    cartCtx.removeItem(id);
+    
+  }
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
