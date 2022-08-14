39306c5285e9e9a88c4574109ff1626cee09e0bc
dhruvchodha7 <dhruvchodha@live.com>
2022-08-14 17:56:05 +0530

    completed use-memo

--- hooks/use-memo/src/App.js
+++ hooks/use-memo/src/App.js
@@ -1,12 +1,40 @@
-
-import './App.css';
+import React, { useState, useMemo, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
+  useEffect(() => {
+    console.log('Theme Changed')
+  }, [themeStyles]);
   return (
     <>
-      
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </>
   );
+}
+
+function slowFunction(number) {
+  console.log("slowFunction");
+  for (let i = 0; i < 1e9; i++) {}
+  return number * 2;
 }
 
 export default App;
