b12effe19d43c5827c241336649bd54bab6d6e54
dhruvchodha7 <dhruvchodha@live.com>
2022-08-16 00:07:46 +0530

    implemented use red

--- hooks/use-ref/src/App.js
+++ hooks/use-ref/src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './App.css';
 
 function App() {
   const [name, setName] = useState('')
+  const renderCount = useRef(0)
+  useEffect(() => {
+    renderCount.current++;
+  });
   return (
     <>
       <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <h1>My name is {name}</h1>
+      <h1>I rendered {renderCount.current}</h1>
     </>
   );
 }
