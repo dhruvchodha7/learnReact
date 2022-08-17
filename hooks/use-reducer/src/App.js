69af9cbfaedf4e830c2527648c04a45d032ca216
dhruvchodha7 <dhruvchodha@live.com>
2022-08-17 17:41:46 +0530

    added reducer function and action

--- hooks/use-reducer/src/App.js
+++ hooks/use-reducer/src/App.js
@@ -1,33 +1,26 @@
-import React, { useState, useReducer } from "react";
+import React, { useReducer } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0)
   const reducer = (state, action) => {
     switch (action.type) {
       case "increment":
         return state + 1;
       case "decrement":
         return state - 1;
+      case "reset":
+        return 0;
       default:
         return state;
     }
   };
   const [count, dispatch] = useReducer(reducer, 0);
 
-  // function changeCount(amount) {
-  //   setCount(prevCount => prevCount + amount)
-  // }
-
-  function resetCount() {
-    setCount(0);
-  }
-
   return (
     <>
       <span>{count}</span>
-      <button onClick={() => changeCount(1)}>+</button>
-      <button onClick={() => changeCount(-1)}>-</button>
-      <button onClick={() => resetCount()}>Reset</button>
+      <button onClick={dispatch({type: 'increment'})}>+</button>
+      <button onClick={dispatch({type: 'decrement'})}>-</button>
+      <button onClick={dispatch({ type: 'reset' })}>Reset</button>
     </>
   );
 }
