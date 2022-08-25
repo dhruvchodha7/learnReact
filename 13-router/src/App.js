8dab2c46ff01a7db72462de42c20db54c405ce32
dhruvchodha7 <dhruvchodha@live.com>
2022-08-25 19:26:16 +0530

    completed router practice

--- 13-router/src/App.js
+++ 13-router/src/App.js
@@ -1,21 +1,26 @@
-import { Route, Switch } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import MainHeader from './components/MainHeader';
+
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
         <Switch>
-          <Route path="/welcome">
+          <Route path='/' exact>
+            <Redirect to='/welcome' />
+          </Route>
+          <Route path='/welcome'>
             <Welcome />
           </Route>
-          <Route path="/products" exact>
+          <Route path='/products' exact>
             <Products />
           </Route>
-          <Route path="/products/:productId">
+          <Route path='/products/:productId'>
             <ProductDetail />
           </Route>
         </Switch>
@@ -25,3 +30,7 @@ function App() {
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
