68bfbdb68df7df6ca2510d28999632c6fe9e7896
dhruvchodha7 <dhruvchodha@live.com>
2022-08-25 18:51:27 +0530

    added nested routes

--- 13-router/src/pages/Products.js
+++ 13-router/src/pages/Products.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Products = () => {
   return (
     <section>
       <h1>Products</h1>
       <ul>
-        <li>Book</li>
-        <li>Carpet</li>
-        <li>Online Course</li>
+        <li><Link to='/products/p1'>A Book</Link></li>
+        <li><Link to='/products/p2'>car</Link></li>
+        <li><Link to='/products/p3'>Online Course</Link></li>
       </ul>
     </section>
   )
