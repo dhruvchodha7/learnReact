d43c12309104a69b7ff7bc90ac11a497ff957aaf
dhruvchodha7 <dhruvchodha@live.com>
2022-08-25 15:49:48 +0530

    added css files to index and mainheader

--- 13-router/src/components/MainHeader.js
+++ 13-router/src/components/MainHeader.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import classes from 'MainHeader.module.css'
 const MainHeader = () => {
   return (
-    <div>MainHeader</div>
+    <header className={classes.header}>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/welcome">Welcome</Link>
+                </li>
+                <li>
+                    <Link to="/products">Products</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
     
   )
 }
